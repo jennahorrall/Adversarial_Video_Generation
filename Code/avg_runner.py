a51f9731aced1b9ef198be3ab2f76a18d6797587
Jenna Horrall <horrall1@quartz770.llnl.gov>
2019-07-17 11:21:49 -0700

    added run scripts

--- Code/avg_runner.py
+++ Code/avg_runner.py
@@ -125,7 +125,7 @@ def main():
     load_path = None
     test_only = False
     num_test_rec = 1  # number of recursive predictions to make on test
-    num_steps = 100
+    num_steps = 50000
     try:
         opts, _ = getopt.getopt(sys.argv[1:], 'l:t:r:a:n:s:OTH',
                                 ['load_path=', 'test_dir=', 'recursions=', 'adversarial=', 'name=',
--- Code/submit_avg_runner.sh
+++ Code/submit_avg_runner.sh
@@ -9,11 +9,13 @@ conda activate py27tens12
 # set log files
 AVG_RUNNER_LOG="avg_runner.log"
 
-# run script
-python avg_runner.py 
-
-
 
 chmod u+rwx *
+
+
+# run script
+python avg_runner.py -t /p/lscratchh/kochansk/summer2019/rescal-snow/test_gaussian_parallel/Test/ -r 2 -n rescal_24h_r2_7_16 -O --stats_freq 100 --summary_freq 100 --img_save_freq 500 --test_freq 2500 --model_save_freq 5000 > $AVG_RUNNER_LOG
+
+
 
 
